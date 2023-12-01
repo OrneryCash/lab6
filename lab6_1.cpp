3bfbb59479c7eef59ec0cdcf8fd9e480e5a06af4
OrneryCash <60810240+OrneryCash@users.noreply.github.com>
2023-12-01 14:45:22 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+  int input;
+  cout << "Enter an integer: ";
+  cin >> input;
+  int even = 0;
+  int odd = 0;
+
+  while (input != 0) {
+    if (input % 2 == 0) even++;
+    else odd++;
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin >> input;
+  }
+  cout << "#Even numbers = " << even << '\n';
+  cout << "#Odd numbers = " << odd;
+  return 0;
 }
